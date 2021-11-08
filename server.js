3ef4993be738596f4b7f13cacc6a70defe611c04
David <n.dave.hernandez@gmail.com>
2021-11-08 01:20:22 -0500

    moved location of server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const app = require("./app");
-const { PORT } = require("./config");
+const app = require("./backend/app");
+const { PORT } = require("./backend/config");
 
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
